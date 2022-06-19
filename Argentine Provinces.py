a10418f3875b1a9600fa25d444d578a65f682663
Erik Huß <107818060+ehub-96@users.noreply.github.com>
2022-06-19 17:44:40 -0600

    Add files via upload

---
+++ Argentine Provinces.py
@@ -0,0 +1,28 @@
+provinces = [ 'BA', 'CA', 'CH', 'CT', "CB", "CB", "CR", "ER", "FO", "JY", "LP", "LR", "MZ", "MI", "NQN", "RN", "SA", "SJ", "SL", "SC", "SF", "SE", "TF", "TU"]
+
+provinces = {
+
+    'BA': 'Buenos Aires',
+    'CA': 'Catamarca',
+    'CH': 'Chaco' ,
+    'CT': 'Chubut',
+    'CB': 'Cordoba',
+    'CR': 'Corrientes',
+    'ER': 'Entre Rios',
+    'FO': 'Formosa',
+    'JY': 'Jujuy',
+    'LP': 'La Pampa',
+    'LR': 'La Rioja',
+    'MZ': 'Mendoza',
+    'MI': 'Misiones',
+    'NQN': 'Neuquen',
+    'RN': 'Rio Negro',
+    'SA': 'Salta',
+    'SJ': 'San Juan',
+    'SL': 'San Luis',
+    'SC': 'Santa Cruz',
+    'SF': 'Santa Fe',
+    'SE': 'Santiago del Estero',
+    'TF': 'Tierra del Fuego',
+    'TU': 'Tucuman',
+}
